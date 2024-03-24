cc45fb08ab3714d330c941f71ec7bae04db409bf
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2024-03-24 18:17:24 +0000

    LongToString Conversion

---
+++ LongToString.java
@@ -0,0 +1,9 @@
+public class LongToString {
+    public static void main(String args[])
+    {
+        Long varLong = 999999999999L;
+        String str = varLong + " ";
+        System.out.println("Converted type : "+ str.getClass().getName());
+        System.out.println(str);
+    }
+}
